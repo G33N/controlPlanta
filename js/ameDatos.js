ae87ae2de4a4aff45bbd9025efff3298de8708f9
G33N <mceliz@enlaceit.com.ar>
2016-10-13 23:09:03 -0300

    Se muestran todos los datos, falta agregar paginacion a tablas y corregir Test Color

--- js/ameDatos.js
+++ js/ameDatos.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
     listarSalidaPlanta();
+    listarStockPlanta();
+    listarStockSalidaPlanta();
 });
 
 function listarSalidaPlanta() {
@@ -19,6 +21,46 @@ function listarSalidaPlanta() {
                 cloroResidual = objeto[i].cloro_residual;
                 fechaHora = objeto[i].fecha_hora;
                 $('#listarSalidaPlanta').append('<tr><td>' + caudal + '</td><td>' + turbiedadEntrada + '</td><td>' + turbiedadSalida + '</td><td>' + cloroResidual + '</td><td>' + fechaHora + '</td></tr>');
+            }
+        }
+    });
+}
+function listarStockPlanta() {
+    $.ajax({
+        type: 'POST',
+        url: "amePlanta.php",
+        data: $(this).serialize() + "&orden=" + 11,
+        success: function(data) {
+            $('#listarStockPlanta').html("");
+            json = jQuery.parseJSON(data);
+            var objeto = json;
+            for (i = 0; i <= (objeto.length - 1); i++) {
+                id = objeto[i].id;
+                sulfato = objeto[i].sulfato;
+                hipoclorito = objeto[i].hipoclorito;
+                polielectrolito = objeto[i].polielectrolito;
+                fechaHora = objeto[i].fecha_hora;
+                $('#listarStockPlanta').append('<tr><td>' + sulfato + '</td><td>' + hipoclorito + '</td><td>' + polielectrolito + '</td><td>' + fechaHora + '</td></tr>');
+            }
+        }
+    });
+}
+function listarStockSalidaPlanta() {
+    $.ajax({
+        type: 'POST',
+        url: "amePlanta.php",
+        data: $(this).serialize() + "&orden=" + 12,
+        success: function(data) {
+            $('#listarStockSalidaPlanta').html("");
+            json = jQuery.parseJSON(data);
+            var objeto = json;
+            for (i = 0; i <= (objeto.length - 1); i++) {
+                id = objeto[i].id;
+                sulfato = objeto[i].sulfato;
+                hipoclorito = objeto[i].hipoclorito;
+                polielectrolito = objeto[i].polielectrolito;
+                fechaHora = objeto[i].fecha_hora;
+                $('#listarStockSalidaPlanta').append('<tr><td>' + sulfato + '</td><td>' + hipoclorito + '</td><td>' + polielectrolito + '</td><td>' + fechaHora + '</td></tr>');
             }
         }
     });
