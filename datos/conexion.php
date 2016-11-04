8516049dfa64118f6a2e50b8f447d78a818390dc
G33N <mceliz@enlaceit.com.ar>
2016-11-04 20:10:29 -0300

    Eliminando archivos

--- datos/conexion.php
+++
@@ -1,6 +0,0 @@
-<?php
-//$conexion = mysqli_connect("mysql.hostinger.com.ar","u140857914_aguas","Seighei7","u140857914_aguas");
-$conexion = mysqli_connect("localhost","root","Seighei7","u140857914_aguas");
-  mysqli_query($conexion,"SET NAMES utf8");
-	 mysqli_query($conexion,"SET CHARACTER_SET utf");
-?>
